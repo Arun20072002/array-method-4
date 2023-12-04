45da6a7051ec8ee9dceb6e08e96d6783d218858e
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:11:24 +0530

    Array-method-4

---
+++ Array-method-4.js
@@ -0,0 +1,8 @@
+document.write("4.Add number 7 at the end of the given array"+"<br>")
+document.write("------------------------------------------"+"<br>")
+
+var b = [1, 2, 3, 4, 5, 6];
+document.write(b+"<br>")
+
+b.push(7);
+document.write(b+"<br>")
